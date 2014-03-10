267eb63765995ac1f41912e7ea132c13d8bad026
Tim Molter <tim.molter@gmail.com>
2014-03-10 17:48:45 +0100

    new UCSD Anomaly dataset

---
+++ datasets-ucsd-anomaly/README.md
@@ -0,0 +1,23 @@
+## Xeiam Datasets UCSD
+
+[raw data](http://www.svcl.ucsd.edu/projects/anomaly/dataset.html) 
+
+## How to Generate Java Data
+
+1. Download raw data from here: http://www.svcl.ucsd.edu/projects/anomaly/dataset.html
+
+1. put all files in `raw` folder in project root
+
+1. Convert tifs to pngs because Java won't work with these tifs for some reason
+
+    find . -iname "*.tif" -type f -exec sh -c 'sips -s format png "$0" --out "${0%.tif}.png"' {} \;
+
+1. Run `Data2DB.java`. 
+
+1. The database containing the data will appear in `db`.
+
+1. Manually copy the `DB_UCSD_ANOMALY.*` files into the `src/main/resources` folder. There should be three files. 
+
+## How to build jar containing Data
+
+1. using Maven, run `mvn clean install`
---
+++ datasets-ucsd-anomaly/src/main/java/com/xeiam/datasets/ucsdanomaly/UCSDAnomaly.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2013-2014 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.xeiam.datasets.ucsdanomaly;
+
+import java.sql.Blob;
+
+/**
+ * @author timmolter
+ */
+public class UCSDAnomaly {
+
+  private int id;
+  private int tifid;
+  private Blob tifbytes;
+  private boolean isanomaly;
+
+  public int getId() {
+
+    return id;
+  }
+
+  public void setId(int id) {
+
+    this.id = id;
+  }
+
+  public int getTifid() {
+
+    return tifid;
+  }
+
+  public void setTifid(int tifid) {
+
+    this.tifid = tifid;
+  }
+
+  public Blob getTifbytes() {
+
+    return tifbytes;
+  }
+
+  public void setTifbytes(Blob tifbytes) {
+
+    this.tifbytes = tifbytes;
+  }
+
+  public boolean isIsanomaly() {
+
+    return isanomaly;
+  }
+
+  public void setIsanomaly(boolean isanomaly) {
+
+    this.isanomaly = isanomaly;
+  }
+
+  @Override
+  public String toString() {
+
+    return "UCSDAnomaly [id=" + id + ", tifid=" + tifid + ", isanomaly=" + isanomaly + "]";
+  }
+
+}
